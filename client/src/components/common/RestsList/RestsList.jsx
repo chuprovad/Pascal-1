d089bede2e0aed568396a83b9ef8ea99a1d61258
Anton Vinogradov <nello1989@mail.ru>
2022-01-16 13:27:30 +0300

    Delete unusables strings in RestList

--- client/src/components/common/RestsList/RestsList.jsx
+++ client/src/components/common/RestsList/RestsList.jsx
@@ -3,27 +3,16 @@ import {useDispatch, useSelector} from "react-redux";
 import {addManyTodos} from "../../../redux/actions/action";
 import axios from "axios";
 import OneRest from "../OneResr/OneRest";
+import { v4 as uuidv4 } from 'uuid';
 
 const RestsList = () => {
 
     const rests = useSelector(state => state.rests)
     const dispatch = useDispatch()
 
-
-    const downloadHandler = async(e) => {
-        e.preventDefault()
-        const response = await axios('https://jsonplaceholder.typicode.com/todos')
-        const restsFromServer = await response.data
-        dispatch(addManyTodos(restsFromServer))
-    }
-
-
-
     return (
         <div>
-            <button onClick={downloadHandler} >Download todos</button>
-
-            {rests && rests.map(rest => <OneRest id={rest.id} title={rest.title} category={rest.category} />
+            {rests && rests.map(rest => <OneRest key={uuidv4()} id={rest.id} title={rest.title} category={rest.category} />
                 )}
         </div>
     );
